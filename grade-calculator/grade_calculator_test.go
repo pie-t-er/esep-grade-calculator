ff982645f90200aac57804a6eecdbfae8492af5d
pie-t-er <alleylofton@ufl.edu>
2025-10-02 23:50:07 -0400

    Added tests to reach 100% code coverage

--- grade-calculator/grade_calculator_test.go
+++ grade-calculator/grade_calculator_test.go
@@ -49,3 +49,55 @@ func TestGetGradeF(t *testing.T) {
 		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
 	}
 }
+
+func TestGetGradeD(t *testing.T) {
+	expected_value := "D"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 60, Assignment)
+	gradeCalculator.AddGrade("memo: failure analysis", 62, Assignment)
+	gradeCalculator.AddGrade("midterm exam", 64, Exam)
+	gradeCalculator.AddGrade("final exam", 65, Exam)
+	gradeCalculator.AddGrade("essay on ai ethics", 68, Essay)
+	gradeCalculator.AddGrade("essay on buffer overflow", 70, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGetGradeC(t *testing.T) {
+	expected_value := "C"
+
+	gradeCalculator := NewGradeCalculator()
+
+	gradeCalculator.AddGrade("open source assignment", 75, Assignment)
+	gradeCalculator.AddGrade("exam 1", 75, Exam)
+	gradeCalculator.AddGrade("", 0, Essay)
+
+	actual_value := gradeCalculator.GetFinalGrade()
+
+	if expected_value != actual_value {
+		t.Errorf("Expected GetGrade to return '%s'; got '%s' instead", expected_value, actual_value)
+	}
+}
+
+func TestGradeType_ToString(t *testing.T) {
+    gt := GradeType(0)
+    actual_value := gt.String()
+    expected_value := "assignment"
+    if actual_value != expected_value {
+        t.Errorf("gt.String() = %s; want %s", actual_value, expected_value)
+    }
+
+    gt = GradeType(1)
+    actual_value = gt.String()
+    expected_value = "exam"
+    if actual_value != expected_value {
+        t.Errorf("gt.String() = %s; want %s", actual_value, expected_value)
+    }
+}
+
